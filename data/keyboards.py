4d4daa064bdcf84060247a758e056d4c0fdde966
AlkoKovad <aleksandr.kopeykin@gmail.com>
2022-09-26 13:02:46 +0300

    Not finished homework upload, but it gets info

--- data/keyboards.py
+++ data/keyboards.py
@@ -2,10 +2,9 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 def menu(message_id):
-    literature_btn = InlineKeyboardButton('Учебная литература', callback_data=f'literature {message_id}')
     docs_btn = InlineKeyboardButton('Материалы по занятиям', callback_data=f'docs {message_id}')
-    homework_btn = InlineKeyboardButton('Домашка', callback_data=f'homework {message_id}')
-    return InlineKeyboardMarkup(row_width=1).add(literature_btn, docs_btn, homework_btn)
+    homework_btn = InlineKeyboardButton('Домашка', callback_data=f'topical_homework {message_id}')
+    return InlineKeyboardMarkup(row_width=1).add(docs_btn, homework_btn)
 
 
 def subjects(message_id, info_type):
@@ -17,8 +16,21 @@ def subjects(message_id, info_type):
 
 
 def materials_by_subject(message_id, info_type):
-    materials_btn = InlineKeyboardButton('материалы к дз', callback_data=f'{info_type}-materials {message_id}')
+    literature_btn = InlineKeyboardButton('Учебная литература', callback_data=f'{info_type}-literature {message_id}')
     homework_btn = InlineKeyboardButton('Д/З', callback_data=f'{info_type}-homework {message_id}')
     done_homework_btn = InlineKeyboardButton('ГДЗ', callback_data=f'{info_type}-done_homework {message_id}')
     return_btn = InlineKeyboardButton('Меню', callback_data=f'menu {message_id}')
-    return InlineKeyboardMarkup(row_width=1).add(materials_btn, homework_btn, done_homework_btn, return_btn)
+    return InlineKeyboardMarkup(row_width=1).add(literature_btn, homework_btn, done_homework_btn, return_btn)
+
+
+def up_down__load_files(message_id, info_type):
+    upload_btn = InlineKeyboardButton('Загрузить файлы', callback_data=f'{info_type}-upload {message_id}')
+    download_btn = InlineKeyboardButton('Скачать файлы', callback_data=f'{info_type}-download {message_id}')
+    return_btn = InlineKeyboardButton('Меню', callback_data=f'menu {message_id}')
+    return InlineKeyboardMarkup(row_width=1).add(upload_btn, download_btn, return_btn)
+
+
+def homework_for_dates(message_id, info_type, dates):
+    date_btns = [InlineKeyboardButton(f'{date}', callback_data=f'{info_type}-{date} {message_id}')
+                 for date in dates]
+    return InlineKeyboardMarkup(row_width=1).add(*[btn for btn in date_btns])
--- main.py
+++ main.py
@@ -2,20 +2,29 @@ import logging
 import os
 
 from aiogram import Bot, Dispatcher, types, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 from dotenv import load_dotenv
 
 import data.keyboards as kb  # do your keyboards HERE!
 from data.db_session import global_init, create_session
 from data.groups import Groups
+from data.homework import Homework
 
 if os.path.exists('.env'):
-    load_dotenv('.env')  # call me if you don't have one
+    load_dotenv('.env')  # call AlkoKovad if you don't have one
 
 BOT_KEY = os.getenv('API_TOKEN')
 
+storage = MemoryStorage()
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=BOT_KEY)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
+
+
+class HomeworkSendState(StatesGroup):
+    homework_files_state = State()
 
 
 @dp.message_handler(commands=['start', 'menu'])
@@ -39,6 +48,10 @@ async def subject_menu(callback_query: types.CallbackQuery):
     There is a data format for reply markups which looks like:
     Section-subject-kind_of_docs"""
 
+    db_sess = create_session()
+    homework_list = db_sess.query(Homework).all()
+    dates = [homework.deadline for homework in homework_list]
+
     info_type, bot_msg = callback_query.data.split()[0], int(callback_query.data.split()[1]) + 1
     # this string could fuck up our bot one day, search "last_message" or smth like that command plz...
 
@@ -49,11 +62,37 @@ async def subject_menu(callback_query: types.CallbackQuery):
         await bot.send_message(callback_query.from_user.id, 'Меню', reply_markup=kb.menu(bot_msg))
         return  # menu exit
     if 'docs-' in info_type:
+        if len(info_type.split('-')) == 3:
+            await bot.send_message(callback_query.from_user.id, 'Выбери действие',
+                                   reply_markup=kb.up_down__load_files(bot_msg, info_type))
+            return
+        if len(info_type.split('-')) == 4:
+
+            if 'upload' in info_type:
+                await bot.send_message(callback_query.from_user.id,
+                                       'Укажи дату в формате DD.MM.YY и приложи файлы')
+                await HomeworkSendState.homework_files_state.set()
+                return  # upload homework entry
+            if 'download' in info_type:
+                await bot.send_message(callback_query.from_user.id, 'Ваше ДЗ по дедлайнам хозяин',
+                                       reply_markup=kb.homework_for_dates(bot_msg, info_type, dates))
+                return  # download homework entry
+
         await bot.send_message(callback_query.from_user.id, 'Выбери материалы',
-                               reply_markup=kb.materials_by_subject(bot_msg, info_type.split('-')[0]))
+                               reply_markup=kb.materials_by_subject(bot_msg, info_type))
+
         return  # materials section entry
     await bot.send_message(callback_query.from_user.id, 'Выбери предмет',
                            reply_markup=kb.subjects(bot_msg, info_type.split('-')[0]))  # subject entry
+
+
+@dp.message_handler(state=HomeworkSendState.homework_files_state)
+async def upload_homework(message: types.message, state: FSMContext):
+    await state.update_data(homework=message)
+    data = await state.get_data()
+    await bot.send_message(message.chat.id, f'{data["homework"]} вот твоя хуйня')
+    await bot.send_message(message.chat.id, f'{search_user_in_groups(data["homework"].from_user.username)} вот тебе сука')
+    await state.finish()
 
 
 @dp.message_handler(commands=['alarm'])
@@ -74,7 +113,8 @@ async def usernames_capture(message: types.Message):
     db_sess = create_session()
     try:
         group = db_sess.query(Groups).filter(Groups.group_chat_id == message.chat.id).first()
-        if not group:
+        if not group and message.chat.id != search_user_in_groups(message.from_user.username) and \
+                message.chat.id[0] == '-':
             group = Groups(group_chat_id=message.chat.id)
             db_sess.add(group)
         if '@' + message.from_user.username + ' ' not in group.members and message.from_user.username != 'None':
@@ -84,6 +124,14 @@ async def usernames_capture(message: types.Message):
     except Exception as e:
         print(e)
     db_sess.commit()
+
+
+def search_user_in_groups(username):
+    db_sess = create_session()
+    groups = db_sess.query(Groups).all()
+    for group in groups:
+        if username in group.members:
+            return group.group_chat_id
 
 
 def db_connect():
